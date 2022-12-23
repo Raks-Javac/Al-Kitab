56fca0a8fba9b59750554106d8757dbedfffbf03
Raks-Javac <rufaikudus2014@gmail.com>
2022-12-23 03:32:08 +0100

    ref:

--- lib/src/features/dashboard/main_dash.dart
+++ lib/src/features/dashboard/main_dash.dart
@@ -27,68 +27,78 @@ class _MainDashBoardViewState extends State<MainDashBoardView>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: TabBarView(controller: _tabController, children: [
-        AlKitabHomeView(),
-        Container(),
-        Container(),
-      ]),
-      bottomNavigationBar: Container(
-        margin: EdgeInsets.all(30),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(20),
-            topRight: Radius.circular(20),
-            bottomLeft: Radius.circular(20),
-            bottomRight: Radius.circular(20),
-          ),
-          color: KColors.semiWhite,
-        ),
-        padding: EdgeInsets.all(10),
-        height: 70,
-        child: TabBar(
-            onTap: (value) {
-              changeActiveIndexstate(value);
-            },
-            unselectedLabelColor: Theme.of(context).primaryColor,
-            splashFactory: NoSplash.splashFactory,
-            indicatorWeight: 0.02,
-            controller: _tabController,
-            tabs: [
+      body: Stack(
+        children: [
+          TabBarView(controller: _tabController, children: [
+            AlKitabHomeView(),
+            Container(),
+            Container(),
+          ]),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
               Container(
+                margin: EdgeInsets.all(30),
                 decoration: BoxDecoration(
-                  color: activeIndex == 0
-                      ? KColors.primaryColor
-                      : KColors.semiWhite,
-                  borderRadius: BorderRadius.circular(200),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                  ),
+                  color: KColors.semiWhite,
                 ),
-                width: 80,
-                height: 80,
-                child: Icon(Icons.home),
+                padding: EdgeInsets.all(10),
+                height: 70,
+                child: TabBar(
+                    onTap: (value) {
+                      changeActiveIndexstate(value);
+                    },
+                    unselectedLabelColor: Theme.of(context).primaryColor,
+                    splashFactory: NoSplash.splashFactory,
+                    indicatorWeight: 0.02,
+                    controller: _tabController,
+                    tabs: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: activeIndex == 0
+                              ? KColors.primaryColor
+                              : KColors.semiWhite,
+                          borderRadius: BorderRadius.circular(200),
+                        ),
+                        width: 80,
+                        height: 80,
+                        child: Icon(Icons.home),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: activeIndex == 1
+                              ? KColors.primaryColor
+                              : KColors.semiWhite,
+                          borderRadius: BorderRadius.circular(200),
+                        ),
+                        width: 80,
+                        height: 80,
+                        child: Icon(Icons.home),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: activeIndex == 2
+                              ? KColors.primaryColor
+                              : KColors.semiWhite,
+                          borderRadius: BorderRadius.circular(200),
+                        ),
+                        width: 80,
+                        height: 80,
+                        child: Icon(Icons.home),
+                      ),
+                    ]),
               ),
-              Container(
-                decoration: BoxDecoration(
-                  color: activeIndex == 1
-                      ? KColors.primaryColor
-                      : KColors.semiWhite,
-                  borderRadius: BorderRadius.circular(200),
-                ),
-                width: 80,
-                height: 80,
-                child: Icon(Icons.home),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                  color: activeIndex == 2
-                      ? KColors.primaryColor
-                      : KColors.semiWhite,
-                  borderRadius: BorderRadius.circular(200),
-                ),
-                width: 80,
-                height: 80,
-                child: Icon(Icons.home),
-              ),
-            ]),
+            ],
+          )
+        ],
       ),
+      // bottomNavigationBar:
     );
   }
 }
